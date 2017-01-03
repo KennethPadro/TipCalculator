d6f53daefef682445bc78903e04484cae6434fc5
Kenneth Padro <kbpadro@gmail.com>
2017-01-03 18:22:32 -0400

    Added loop to display radio buttons

--- tip_calc.php
+++ tip_calc.php
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <body>
+
 <div class="w3-container w3-margin-top " style="max-width:500px;">
     <div class="w3-card-4 " >
         <header class="w3-container w3-teal">
@@ -11,11 +12,17 @@
         </header>
         <div class="w3-container w3-margin">
             <form action="" method="post">
-                Sub Total: $<input type="text" name="subTotal" value=""/><br/><br/>
+                Sub Total: $<input type="text" name="subTotal" value="<?php echo $_POST['subTotal']; ?>"/><br/><br/>
                 Select tip Percentage:<br/>
-                    <input type="radio" name="tip" value= />
 
-
+                <?php
+                $values = array(10, 15, 20);
+                foreach ($values as $value) {
+                    ?>
+                    <input type="radio" name="tip" value=<?php echo '.' . $value; ?>><?php echo $value; ?>%<br>
+                    <?php
+                } ?>
+                <br>
                 <input type="submit" name="submit"class="w3-btn w3-teal"  value="Calculate Tip">
                 <br>
         </div>
