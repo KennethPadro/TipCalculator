d8bdac3cc9292f401777535fe33569ff271aa3f4
Kenneth Padro <kbpadro@gmail.com>
2017-01-03 18:21:38 -0400

    Added form

--- tip_calc.php
+++ tip_calc.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kbpad
- * Date: 1/3/2017
- * Time: 6:14 PM
- */
+<!DOCTYPE html>
+<html>
+<title>Tip Calculator</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<body>
+<div class="w3-container w3-margin-top " style="max-width:500px;">
+    <div class="w3-card-4 " >
+        <header class="w3-container w3-teal">
+            <h1>Tip Calculator</h1>
+        </header>
+        <div class="w3-container w3-margin">
+            <form action="" method="post">
+                Sub Total: $<input type="text" name="subTotal" value=""/><br/><br/>
+                Select tip Percentage:<br/>
+                    <input type="radio" name="tip" value= />
+
+
+                <input type="submit" name="submit"class="w3-btn w3-teal"  value="Calculate Tip">
+                <br>
+        </div>
+</body>
+</html>
